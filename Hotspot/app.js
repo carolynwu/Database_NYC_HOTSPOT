77149009c0b4797685a1b19441ee20684e3debed
carolynwu <wuxujia199438@gmail.com>
2018-04-09 11:19:10 -0500

    connect express and mysql

--- Hotspot/app.js
+++ Hotspot/app.js
@@ -1,8 +1,23 @@
 var express= require("express");
+var mysql=require("mysql");
 var app= express();
 
+var connection=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"0000",
+    database:"test"
+});
+
 app.get("/",function(req,res){
-    res.send("You have reached the home page!");
+    // find count of  object ID in DB
+    var q="SELECT COUNT(*) AS total FROM account";
+    connection.query(q,function(err, result){
+       if (err) throw err;
+     var count=result[0].total;
+
+     res.send("We have "+ count + " users in ");
+    });
 });
 
 app.listen(8080, function(){
