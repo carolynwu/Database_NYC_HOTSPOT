e01bf4a4424f6388de16513f92071963738bb02d
carolynwu <wuxujia199438@gmail.com>
2018-04-23 10:35:30 -0500

    express get all borough from database

--- Hotspot/app.js
+++ Hotspot/app.js
@@ -1,6 +1,10 @@
+
 var express= require("express");
+const cors=require("cors");
+
 var mysql=require("mysql");
 var app= express();
+app.use(cors());
 
 var connection=mysql.createConnection({
     host:"localhost",
@@ -9,16 +13,42 @@ var connection=mysql.createConnection({
     database:"test"
 });
 
-app.get("/",function(req,res){
-    // find count of  object ID in DB
-    var q="SELECT COUNT(*) AS total FROM account";
-    connection.query(q,function(err, result){
-       if (err) throw err;
-     var count=result[0].total;
+app.get("/",function(req,res) {
+    res.send("connected")
+});
 
-     res.send("We have "+ count + " users in ");
+app.get("/chart",function(req,res){
+    // find count of  object ID in DB
+     var b= "SELECT COUNT(*) AS brooklyn_wifi FROM account WHERE Borough_NAME='Brooklyn'";
+     var q= "SELECT COUNT(*) AS queens_wifi FROM account WHERE Borough_NAME='Queens' ";
+     var m= "SELECT COUNT(*) AS Manhattan_wifi FROM account WHERE Borough_NAME='Manhattan' ";
+
+    connection.query(b,function(err, result){
+        if (err) throw err;
+        else{
+            return res.json({
+                data:result
+            })
+        }
+    });
+    connection.query(q,function(err, result){
+        if (err) throw err;
+        else{
+            return res.json({
+                data:result
+            })
+        }
+    });
+    connection.query(m,function(err, result){
+        if (err) throw err;
+        else{
+            return res.json({
+                data:result
+            })
+        }
     });
 });
+
 
 app.listen(8080, function(){
     console.log("Server running on 8080!");
